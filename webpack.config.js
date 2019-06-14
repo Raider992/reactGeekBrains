80a1670c648ba03bbd15646ff15f3bbee5acc24d
Raider992 <Raider992@users.noreply.github.com>
2019-06-14 11:56:43 +0300

    первый комит

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
+                resolve: { extensions: [".js", ".jsx"] },
                 use: "babel-loader"
             },
         ]
