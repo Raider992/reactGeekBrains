c3e82fa50dc646b287b04998690477223fd75e00
Raider992 <Raider992@users.noreply.github.com>
2019-06-14 14:33:58 +0300

    второй комит

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter} from "react-router-dom";
 
-import Fio from './containers/Fio'
+
+import Header from './containers/Header'
 
 class App extends Component {
     render() {
         return (
-            <>
-                <Fio />
-            </>
+            <BrowserRouter>
+                <Header/>
+            </BrowserRouter>
         );
     }
 }
