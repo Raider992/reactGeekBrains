23e6dc9d51a35587d43ebc8a688c2a3859ca0d3f
Raider992 <Raider992@users.noreply.github.com>
2019-06-17 17:14:08 +0300

    1234

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
@@ -15,6 +15,10 @@ module.exports = {
                 resolve: { extensions: [".js", ".jsx"] },
                 use: "babel-loader"
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
         ]
     }
 };
